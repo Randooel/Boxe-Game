e80c149b2fec515e0d29bc3e58b1750a4464e851
Randooel <leonardo.alves.barros.1@gmail.com>
2024-11-01 16:28:36 -0300

    Setup do projeto
    - Sprites, scripts e objetos setados na cena

---
+++ Boxe Game/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] int vidaMax;
+    [SerializeField] int vidaAtual;
+
+    [SerializeField] int danoFraco;
+    [SerializeField] int danoForte;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
